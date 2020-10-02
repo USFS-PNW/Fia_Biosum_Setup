fd773cdf59b4e1ec7ecfd37173970c0a479de971
lbross <lbross@pdx.edu>
2020-10-02 11:29:29 -0700

    Add files for interim ANL-FCS solution; Also start work on install guide

---
+++ fcs/fcs_tree_calc.bat
@@ -0,0 +1,62 @@
+@ECHO OFF
+TITLE = FCS Volume and Biomass Compiler
+
+SET SHOWMSG=%1
+
+IF EXIST "%APPDATA%"\FIABiosum\fcs_error_msg.txt DEL "%APPDATA%"\FIABiosum\fcs_error_msg.txt
+
+CALL JAVA.EXE -jar "%APPDATA%"/FIABiosum/BiosumComps.jar "%APPDATA%"/FIABiosum/FCS_TREE.DB
+SET ERRNUM=%ERRORLEVEL%
+
+IF "%ERRNUM%"=="1" GOTO tryagain
+IF "%ERRNUM%"=="-1" GOTO errdb
+IF "%ERRNUM%"=="-2" GOTO errtree
+IF "%ERRNUM%"=="0" GOTO success
+
+:tryagain
+IF NOT EXIST C:\JDK1.8\BIN\JAVA.EXE GOTO errjava
+CALL C:\JDK1.8\BIN\JAVA.EXE -jar "%APPDATA%"/FIABiosum/BiosumComps.jar "%APPDATA%"/FIABiosum/FCS_TREE.DB
+SET ERRNUM=%ERRORLEVEL%
+
+
+IF "%ERRNUM%"=="1" GOTO errjava
+IF "%ERRNUM%"=="-1" GOTO errdb
+IF "%ERRNUM%"=="-2" GOTO errtree
+IF "%ERRNUM%"=="0" GOTO success
+
+GOTO errunk
+
+:errdb
+echo msgbox "SQLite database file not found",0,"FCS Volume and Biomass Compiler" > %tmp%\biosum_err_msg.vbs
+IF "%SHOWMSG%"=="Y" wscript %tmp%\biosum_err_msg.vbs
+del %tmp%\biosum_err_msg.vbs
+GOTO exit
+
+:errtree
+echo msgbox "Not a valid tree count",0,"FCS Volume and Biomass Compiler" > %tmp%\biosum_err_msg.vbs
+IF "%SHOWMSG%"=="Y" wscript %tmp%\biosum_err_msg.vbs
+del %tmp%\biosum_err_msg.vbs
+GOTO exit
+
+:errjava
+echo msgbox "Problem detected running JAVA.EXE" + CHR(13) + "32-bit requirements" + CHR(13) + "-----------------------" + CHR(13) + "Install Java 8 Update 251" + CHR(13) + "Install JDK1.8" + CHR(13) + CHR(13) +  "64-bit requirements" + CHR(13) + "-----------------------" + CHR(13) + "Install Java 8 Update 251" + CHR(13) + "Install Oracle 64bit Client" + CHR(13) + "Install JDK1.8 (optional)",0,"FCS Volume and Biomass Compiler" > %tmp%\biosum_err_msg.vbs
+IF "%SHOWMSG%"=="Y" wscript %tmp%\biosum_err_msg.vbs
+del %tmp%\biosum_err_msg.vbs
+echo Problem detected running JAVA.EXE\r\n32-bit requirements\r\n-----------------------\r\nInstall Java 8 Update 251\r\nInstall JDK1.8\r\n\r\n64-bit requirements\r\n-----------------------\r\nInstall Java 8 Update 251\r\nInstall Oracle 64bit Client\r\nInstall JDK1.8 (optional)>"%APPDATA%"\FIABiosum\fcs_error_msg.txt
+GOTO exit
+
+:errunk
+SET /P ERRMSG=<"%APPDATA%"/FIABiosum/FCS_ERROR_MSG.TXT
+echo msgbox "%ERRMSG%",0,"FCS Volume and Biomass Compiler" > %tmp%\biosum_err_msg.vbs
+IF "%SHOWMSG%"=="Y" wscript %tmp%\biosum_err_msg.vbs
+del %tmp%\biosum_err_msg.vbs
+GOTO exit
+
+:success
+echo msgbox "Done with with no errors detected",0,"FCS Volume and Biomass Compiler" > %tmp%\biosum_msg.vbs
+IF "%SHOWMSG%"=="Y" wscript %tmp%\biosum_msg.vbs
+del %tmp%\biosum_msg.vbs
+
+:exit
+IF "%SHOWMSG%"=="" EXIT
+IF "%SHOWMSG%"=="N" EXIT
