92818d03d8ba58c9906d4c408020bcdf43da828c
lbross <lbross@pdx.edu>
2022-04-08 13:08:50 -0700

    issue #262: Fix batch file and release notes

--- fcs/fcs_tree_calc.bat
+++ fcs/fcs_tree_calc.bat
@@ -5,6 +5,9 @@ SET SHOWMSG=%1
 
 IF EXIST "%APPDATA%"\FIABiosum\fcs_error_msg.txt DEL "%APPDATA%"\FIABiosum\fcs_error_msg.txt
 
+IF NOT EXIST "%JAVA_HOME%"\bin\JAVA.EXE GOTO tryagain
+
+REM Update the tryagain section if the arguments change
 CALL "%JAVA_HOME%"\bin\JAVA.EXE -jar "%APPDATA%"/FIABiosum/BiosumComps.jar -tc "%APPDATA%"/FIABiosum/BiosumSpeciesConfig.db ^
                -td "%APPDATA%"/FIABiosum/fcs_tree.db -tdt biosum_calc
 
@@ -16,10 +19,9 @@ IF "%ERRNUM%"=="-2" GOTO errtree
 IF "%ERRNUM%"=="0" GOTO success
 
 :tryagain
-IF NOT EXIST C:\JDK1.8\BIN\JAVA.EXE GOTO errjava
-CALL C:\JDK1.8\BIN\JAVA.EXE -jar "%APPDATA%"/FIABiosum/BiosumComps.jar "%APPDATA%"/FIABiosum/FCS_TREE.DB
+CALL JAVA.EXE -jar "%APPDATA%"/FIABiosum/BiosumComps.jar -tc "%APPDATA%"/FIABiosum/BiosumSpeciesConfig.db ^
+               -td "%APPDATA%"/FIABiosum/fcs_tree.db -tdt biosum_calc
 SET ERRNUM=%ERRORLEVEL%
-
 
 IF "%ERRNUM%"=="1" GOTO errjava
 IF "%ERRNUM%"=="-1" GOTO errdb
@@ -42,10 +44,10 @@ GOTO exit
 
 :errjava
 Rem This section requires a specific version/location for the JDK
-echo msgbox "Problem detected running JAVA.EXE" + CHR(13) + "64-bit requirements" + CHR(13) + "-----------------------" + CHR(13) + "Install Java 8 Update 251" + CHR(13) + "Install Oracle 64bit Client" + CHR(13) + "Install JDK1.8 (optional)",0,"FCS Volume and Biomass Compiler" > %tmp%\biosum_err_msg.vbs
+echo msgbox "Problem detected running JAVA.EXE" + CHR(13) + "64-bit requirements" + CHR(13) + "-----------------------" + CHR(13) + "Install Java 8" + CHR(13) + "Install JDK1.8 (optional)",0,"FCS Volume and Biomass Compiler" > %tmp%\biosum_err_msg.vbs
 IF "%SHOWMSG%"=="Y" wscript %tmp%\biosum_err_msg.vbs
 del %tmp%\biosum_err_msg.vbs
-echo Problem detected running JAVA.EXE\r\n64-bit requirements\r\n-----------------------\r\nInstall Java 8 Update 251\r\nInstall Oracle 64bit Client\r\nInstall JDK1.8 (optional)>"%APPDATA%"\FIABiosum\fcs_error_msg.txt
+echo Problem detected running JAVA.EXE\r\n64-bit requirements\r\n-----------------------\r\nInstall JDK1.8 (optional)>"%APPDATA%"\FIABiosum\fcs_error_msg.txt
 GOTO exit
 
 :errunk
